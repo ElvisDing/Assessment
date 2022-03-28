3f59e67be725be4fa4636e855836fec8e9b5779c
ElvisDing <98984712+ElvisDing@users.noreply.github.com>
2022-03-28 14:23:18 +1300

    Update Example_1.ino
    I added some more coding from library and i will check the errors an bugs in these code.

--- Example_1/Example_1.ino
+++ Example_1/Example_1.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>  // Click here to get the library: http://librarymanager/All#SparkFun_LPS25HB
 OpenLog myLog; 
 
@@ -24,8 +25,32 @@ const byte OpenLogAddress = 42;// default qwiic openLog I2C address
 
 void loop() {
 
-  myLog.prinIn("This goes to the log file");
-  Serial.printIn("This goes to the terminal");  
+  myLog.println ("This goes to the log file");
+  Serial.println ("This goes to the terminal");  
   Serial.print("Pressure in hPa");
-    delay(40);/ Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
+    delay(40);// Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
+}
+myLog.println("This is recorded to the default log file");
+  myLog.append("appendMe.txt");
+  myLog.println("This is recorded to appendMe.txt");
+  myLog.println("If this file doesn't exist it is created and");
+  myLog.println("anything sent to OpenLog will be recorded to this file");
+
+  myLog.println();
+  myLog.println(F("Note: We can use the F(\"\") in sketches to move big print statements into program memory to save RAM"));
+  myLog.append("appendMe1.txt");
+  myLog.println(F("Note: We can use the F(\"\") in sketches to move big print statements into program memory to save RAM"));
+  myLog.println(F("Note: We can use the F(\"\") in sketches to move big print statements into program memory to save RAM"));
+  myLog.syncFile();
+
+  Serial.println("Done!");
+}
+
+void loop()
+{
+  //Blink the Status LED because we're done!
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
 }
