a88eb3ad10fcf456f903d2d68b4cc87350089b8e
ElvisDing <98984712+ElvisDing@users.noreply.github.com>
2022-03-30 11:54:13 +1300

    servo
    I added a servo code for my coding

--- assessment/assessment.ino
+++ assessment/assessment.ino
@@ -1,10 +1,12 @@
 #include <Wire.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>
 #include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
+#include <Servo.h>
 OpenLog myLog; //Create instance
 LPS25HB pressureSensor; // Create an object of the LPS25HB class
+Servo myservo;  // create servo object to control a servo
 int ledPin = LED_BUILTIN; //Status LED connected to digital pin 13
-
+int pos = 0;    // variable to store the servo position
 const byte OpenLogAddress = 42; //Default Qwiic OpenLog I2C address
 
 void setup()
@@ -32,6 +34,7 @@ void setup()
   myLog.syncFile();
 
   Serial.println("Done!");
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
@@ -51,6 +54,6 @@ else
   Serial.println("Disconnected");
   pressureSensor.begin();
 } 
-
   delay(40);                                               // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
+  myservo.write(pos);              // tell servo to go to position in variable 'pos'
 }
