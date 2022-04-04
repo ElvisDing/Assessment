cb81ae1fdb21887acf1fab3c702696d353fb1f5a
ElvisDing <98984712+ElvisDing@users.noreply.github.com>
2022-04-05 11:23:21 +1200

    Update assessment.ino
    I deleted some code that I don't actually need and added some code to make sure the code is working.

--- assessment/assessment.ino
+++ assessment/assessment.ino
@@ -12,25 +12,14 @@ const byte OpenLogAddress = 42; //Default Qwiic OpenLog I2C address
 void setup()
 {
   pinMode(ledPin, OUTPUT);
-
+ pressureSensor.begin();
   Wire.begin();
   myLog.begin(); //Open connection to OpenLog (no pun intended)
 
   Serial.begin(9600); //9600bps is used for debug statements
-  Serial.println("Run OpenLog Append File Test");
-  myLog.println("Run OpenLog Append File Test");
-
-  myLog.println("This is recorded to the default log file");
-  myLog.append("appendMe.txt");
-  myLog.println("This is recorded to appendMe.txt");
-  myLog.println("If this file doesn't exist it is created and");
-  myLog.println("anything sent to OpenLog will be recorded to this file");
-
-  myLog.println();
-  myLog.println(F("Note: We can use the F(\"\") in sketches to move big print statements into program memory to save RAM"));
-  myLog.append("appendMe1.txt");
-  myLog.println(F("Note: We can use the F(\"\") in sketches to move big print statements into program memory to save RAM"));
-  myLog.println(F("Note: We can use the F(\"\") in sketches to move big print statements into program memory to save RAM"));
+ 
+  myLog.append("Elvis.txt");
+ 
   myLog.syncFile();
 
   Serial.println("Done!");
@@ -48,6 +37,10 @@ void loop() {
   Serial.print(pressureSensor.getPressure_hPa());          // Get the pressure reading in hPa
   Serial.print(", Temperature (degC): "); 
   Serial.println(pressureSensor.getTemperature_degC());    // Get the temperature in degrees C
+   myLog.print("Pressure (hPa): "); 
+  myLog.print(pressureSensor.getPressure_hPa());          // Get the pressure reading in hPa
+  myLog.print(", Temperature (degC): "); 
+  myLog.println(pressureSensor.getTemperature_degC());    // Get the temperature in degrees C
 }
 else
 {
