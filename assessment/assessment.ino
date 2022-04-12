6eff104c999dc121cc281f7640a4dc98f247927a
ElvisDing <98984712+ElvisDing@users.noreply.github.com>
2022-04-13 11:53:46 +1200

    Update assessment.ino

--- assessment/assessment.ino
+++ assessment/assessment.ino
@@ -31,14 +31,29 @@ void setup()
 }
 
 void loop() {
-  if (pressureSensor.isConnected() == true);
+  if (pressureSensor.isConnected() == true)
   {
-  if (pressureSensor.getStatus() == 0x00);
+    if (pressureSensor.getStatus() == 0x00)
+    {
+      pressureSensor.begin();
+    } // If it is connected but not responding (for example after a hot-swap) then it may need to be re-initialized
 
-  {
-    Serial.println("Error with pressureSensor");
-    pressureSensor.begin();
+    Serial.print("Pressure (hPa): ");
+    Serial.print(pressureSensor.getPressure_hPa()); // Get the pressure reading in hPa as determined by dividing the number of ADC counts by 4096 (according to the datasheet)
+    Serial.print(", Temperature (degC): ");
+    Serial.println(pressureSensor.getTemperature_degC()); // Get the temperature in degrees C by dividing the ADC count by 480
+
+    myLog.print("Pressure (hPa): ");
+    myLog.print(pressureSensor.getPressure_hPa()); // Get the pressure reading in hPa as determined by dividing the number of ADC counts by 4096 (according to the datasheet)
+    myLog.print(", Temperature (degC): ");
+    myLog.println(pressureSensor.getTemperature_degC()); // Get the temperature in degrees C by dividing the ADC count by 480
+
+    myLog.syncFile();
   }
-  delay(40);                                               // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
+  else
+  {
+    Serial.println("Disconnected");
+    pressureSensor.begin();
+  }                                         // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
   myservo.write(pos);              // tell servo to go to position in variable 'pos'
 }}
