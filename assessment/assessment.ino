480ce5b8c8f7f3e29761db625e64430c07fe4bbd
ElvisDing <98984712+ElvisDing@users.noreply.github.com>
2022-05-16 15:10:11 +1200

    Update assessment.ino
    change to the descriptive name for some  code

--- assessment/assessment.ino
+++ assessment/assessment.ino
@@ -4,62 +4,69 @@
 #include <Servo.h>
 
 
-OpenLog myLog; //Create instance
-LPS25HB pressureSensor; // Create an object of the LPS25HB class
-Servo myservo;  // create servo object to control a servo
-
-
+OpenLog Data; //Create instance
+LPS25HB pressureIndicators; // Create an object of the LPS25HB class
+Servo Rotor;  // create servo object to control a servo
 
 int pos = 0;    // variable to store the servo position
 const byte OpenLogAddress = 42; // Default Qwiic OpenLog I2C address
-const byte SERVO = 11;// 
+const byte SERVO = 11;//
+const byte Delay=3000;
+const byte Angle=90;
 
 void setup()
 {
- 
-  pressureSensor.begin();
+
+  pressureIndicators.begin();
   Wire.begin();
-  myLog.begin(); //Open connection to OpenLog (no pun intended)
+  Data.begin(); //Open connection to OpenLog (no pun intended)
 
   Serial.begin(9600); //9600bps is used for debug statements
-  myLog.create("Elvis.txt");
-  myLog.append("Elvis.txt");
-  myLog.print("Pressure, Temperature");
-  myLog.syncFile();
+  Data.create("Elvis.txt");
+  Data.append("Elvis.txt");
+  Data.print("Pressure, Temperature");
+  Data.syncFile();
 
-/*Pressure, Temperature
- * 25,15
- * 25,300
- */
+  /*Pressure, Temperature
+     25,15
+     25,300
+  */
 
-  Serial.println("set up finished");  
-  myservo.attach(SERVO);  // attaches the servo on pin 9 to the servo object   
+  Serial.println("set up finished");
+  Rotor.attach(SERVO);  // attaches the servo on pin 9 to the servo object
+  Rotor.write(0);
+
 }
 
 void loop() {
-  if (pressureSensor.isConnected() == true)
+  if (pressureIndicators.isConnected() == true)
   {
-    if (pressureSensor.getStatus() == 0x00)
+    if (pressureIndicators.getStatus() == 0x00)
     {
-      pressureSensor.begin();
+      pressureIndicators.begin();
     } // If it is connected but not responding (for example after a hot-swap) then it may need to be re-initialized
 
     Serial.print("Pressure (hPa): ");
-    Serial.print(pressureSensor.getPressure_hPa()); // Get the pressure reading in hPa as determined by dividing the number of ADC counts by 4096 (according to the datasheet)
+    Serial.print(pressureIndicators.getPressure_hPa()); // Get the pressure reading in hPa as determined by dividing the number of ADC counts by 4096 (according to the datasheet)
     Serial.print(", Temperature (degC): ");
-    Serial.println(pressureSensor.getTemperature_degC()); // Get the temperature in degrees C by dividing the ADC count by 480
+    Serial.println(pressureIndicators.getTemperature_degC()); // Get the temperature in degrees C by dividing the ADC count by 480
 
 
-    myLog.print(pressureSensor.getPressure_hPa()); // Get the pressure reading in hPa as determined by dividing the number of ADC counts by 4096 (according to the datasheet)
-    myLog.print(",");
-    myLog.println(pressureSensor.getTemperature_degC()); // Get the temperature in degrees C by dividing the ADC count by 480
+    Data.print(pressureIndicators.getPressure_hPa()); // Get the pressure reading in hPa as determined by dividing the number of ADC counts by 4096 (according to the datasheet)
+    Data.print(",");
+    Data.println(pressureIndicators.getTemperature_degC()); // Get the temperature in degrees C by dividing the ADC count by 480
 
-    myLog.syncFile();
+    Data.syncFile();
   }
   else
   {
     Serial.println("Disconnected");
-    pressureSensor.begin();
-  }                                         // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
-  myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    pressureIndicators.begin();
+  }                                         
+  if (millis() > Delay) {  // Delay(3000) ms corresponds to the maximum update rate of the sensor.
+
+    Serial.print("Parachute deployed");
+    Rotor.write(Angle);              // tell servo to go to position in variable '90'.
+  }
+
 }
